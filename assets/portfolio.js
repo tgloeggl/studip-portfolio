5a5e839f6547f376f242f89cf8794e65f2f387bc
tgloeggl <tgloeggl@uos.de>
2014-05-30 10:34:37 +0200

    unhide tasks if no filter is selected, remove trailing spaces

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -10,12 +10,12 @@ var STUDIP = STUDIP || {};
 
     STUDIP.Portfolio = {
         studiengaenge: null,
-                
+
         getTemplate: _.memoize(function(name) {
             return _.template(jQuery("script." + name).html());
         }),
     };
-    
+
     STUDIP.Portfolio.Homepage = {
         tag1: null,
         tag2: null,
@@ -27,7 +27,7 @@ var STUDIP = STUDIP || {};
                 if ($(this).parent().hasClass('lvl2')) {
                     $(this).siblings().removeClass('selected');
                     $(this).toggleClass('selected');
-                    
+
                     STUDIP.Portfolio.Homepage.tag2 = $(this).hasClass('selected') ? $(this).attr('data-tag') : null;
                 } else {
                     $(this).parent().siblings().find('.open').toggleClass('open', 'closed')
@@ -35,32 +35,39 @@ var STUDIP = STUDIP || {};
 
                     $(this).toggleClass('open', 'closed');
                     $(this).parent().find('.lvl2').toggle();
-                    
+
                     STUDIP.Portfolio.Homepage.tag1 = $(this).hasClass('open') ? null : $(this).attr('data-tag') ;
                     STUDIP.Portfolio.Homepage.tag2 = null;
                 }
-                
+
                 STUDIP.Portfolio.Homepage.filter(STUDIP.Portfolio.Homepage.tag1, STUDIP.Portfolio.Homepage.tag2);
             })
         },
-        
+
         filter: function(tag1, tag2) {
+            // if no tag is select anymore, show everything
             if (tag1 === null && tag2 === null) {
+                // show all tables
                 $('table[data-tag]').show('explode', 800);
+
+                // show all tasks
+                $('table[data-tag] tr.task').show('fade');
                 return;
             }
 
             if (tag1 !== null) {
                 $('table[data-tag]').each(function() {
+                    // toggle the main tag
                     if ($(this).attr('data-tag') === tag1) {
                         $(this).show('fade', 1000);
                     } else {
                         $(this).hide('explode', 800);
                     }
-                    // $(this).toggle($(this).attr('data-tag') === tag1);
-                    
+
+                    // toggle the subtags
                     if (tag2 !== null && $(this).attr('data-tag') === tag1) {
                         $(this).find('tr.task').each(function() {
+                            // toggle correct tasks to show / hide - match their tags
                             if ($.inArray(tag2, $(this).data('tags')) !== -1) {
                                 $(this).show('fade');
                             } else {
@@ -74,7 +81,7 @@ var STUDIP = STUDIP || {};
                 });
             }
         },
-        
+
         addPermission: function() {
             var data = {
                 user: $('#permissions select[name=search]').val(),
@@ -82,40 +89,40 @@ var STUDIP = STUDIP || {};
                 perm: $('#permissions select[name=permission]').val(),
                 permission: $('#permissions select[name=permission]').parent().find('.chosen-single').text()
             };
-            
+
             if (data.user === undefined || data.user === null) {
                 $('#permissions select[name=search]').siblings('.chosen-error').hide();
                 $('#permissions select[name=search]').siblings('.chosen-error').show('highlight');
                 return;
             }
-            
+
             $('#permissions select[name=search]').siblings('.chosen-error').hide();
-            
+
             STUDIP.Portfolio.Homepage.addPermissionTemplate(data);
         },
-        
+
         addPermissionTemplate: function(data) {
             var template = STUDIP.Portfolio.getTemplate('permission');
 
             $('#permission_list').append(template(data)).find('div:last-child img').click(function() {
                 $(this).parent().parent().remove();
-            });  
+            });
         }
     };
 
     STUDIP.Portfolio.Admin = {
         num : 0,
-        
+
         /**
          * add a studycourse-combo select-box to the view
-         * 
+         *
          * @returns {undefined}
          */
         addCombo: function (selected_elements) {
             // load the studycourse if they are not yet present
             if (STUDIP.Portfolio.studiengaenge === null) {
                 $.ajax(STUDIP.URLHelper.getURL('plugins.php/portfolio/admin/set/get_studycourses'), {
-                    success: function(data) { 
+                    success: function(data) {
                         STUDIP.Portfolio.studiengaenge = data;
                         STUDIP.Portfolio.Admin.doAddCombo(selected_elements);
                     }
@@ -125,7 +132,7 @@ var STUDIP = STUDIP || {};
             }
 
         },
-        
+
         doAddCombo: function(selected_elements) {
             var template = STUDIP.Portfolio.getTemplate('studycourse_template');
             var template_data = {
@@ -140,12 +147,12 @@ var STUDIP = STUDIP || {};
                     $('select[data-studycourse-num=' + this.num + '] option[value=' + selected_elements[i] + ']').attr('selected', 'selected');
                 }
             }
-            
+
             $('div.studiengang_combos select').chosen();
-            
+
             this.num++;
         },
-        
+
         /**
          * remove the studygroup-combo denoted by the passed num from the view
          * @param int num
